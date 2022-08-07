3b7fe28a3e1d8cc3d90485b5785fa7cfea2fc84f
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2022-08-07 13:07:13 -0400

    refactor

--- L2 TVL/optimism_app_net_flows.py
+++ L2 TVL/optimism_app_net_flows.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[61]:
+# In[91]:
 
 
 # ! pip install pandas
@@ -12,7 +12,7 @@
 # ! pip freeze = requirements.txt
 
 
-# In[62]:
+# In[101]:
 
 
 import pandas as pd
@@ -20,10 +20,11 @@ import requests as r
 import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime, timedelta
+import numpy as np
 import os
 
 
-# In[63]:
+# In[93]:
 
 
 pwd = os.getcwd()
@@ -33,7 +34,7 @@ else:
     prepend = 'L2 TVL/'
 
 
-# In[64]:
+# In[94]:
 
 
 
@@ -54,118 +55,140 @@ protocols = [
 # print(protocols[0])
 prod = []
 for prot in protocols:
-    ad = pd.json_normalize( r.get(api_str + prot[0]).json()['chainTvls']['Optimism']['tokens'] )
-    ad['date'] = pd.to_datetime(ad['date'], unit ='s') #convert to days
-    ad = pd.melt(ad,id_vars = ['date'])
-    ad = ad.rename(columns={'variable':'token'})
-    ad['token'] = ad['token'].str.replace('tokens.','', regex=False)
-    ad['protocol'] = prot[0]
-    ad['start_date'] = pd.to_datetime(prot[1])
-    ad['date'] = ad['date'] - timedelta(days=1) #change to eod vs sod
-    prod.append(ad)
+    tp = r.get(api_str + prot[0]).json()['chainTvls']['Optimism']
+    ad = pd.json_normalize( tp['tokens'] )
+    ad_usd = pd.json_normalize( tp['tokensInUsd'] )
+    if not ad.empty:
+        ad = pd.melt(ad,id_vars = ['date'])
+        ad = ad.rename(columns={'variable':'token','value':'token_value'})
+        ad_usd = pd.melt(ad_usd,id_vars = ['date'])
+        ad_usd = ad_usd.rename(columns={'variable':'token','value':'usd_value'})
+        ad = ad.merge(ad_usd,on=['date','token'])
+        
+        ad['date'] = pd.to_datetime(ad['date'], unit ='s') #convert to days
+
+        ad['token'] = ad['token'].str.replace('tokens.','', regex=False)
+        ad['protocol'] = prot[0]
+        ad['start_date'] = pd.to_datetime(prot[1])
+        ad['date'] = ad['date'] - timedelta(days=1) #change to eod vs sod
+        prod.append(ad)
 
 df_df = pd.concat(prod)
 df_df
 
 
-# In[65]:
+# In[95]:
 
 
 # df_df[df_df['protocol']=='perpetual-protocol']
 
 
-# In[66]:
+# In[96]:
 
 
-#defillama api feedback - only token symbols come through, makes it hard to map w/o doing it manually
-coingecko_token_map = [
-    ['LINK','chainlink']
-    ,['USDT','tether']
-    ,['USDC','usd-coin']
-    ,['WETH','weth']
-    ,['SUSD','nusd']
-    ,['DAI','dai']
-    ,['AAVE','aave']
-    ,['WBTC','wrapped-bitcoin']
-    ,['SNX','havven']
-    ,['OP','optimism']
-    ,['SETH','seth']
-    ,['FXS','frax-share']
-    ,['THALES','thales']
-    ,['FRAX','frax']
-    ,['ALUSD','alchemix-usd']
-    ,['PERP','perpetual-protocol']
-    ,['LUSD','liquity-usd']
-    ,['LYRA','lyra-finance']
-    ,['HND','hundred-finance']
-    ,['BITANT','bitant']
-    ,['UNI','uniswap']
-    ,['SLINK','slink']
-    ,['VELO','velodrome-finance']
-    ,['DOLA','dola-usd']
-    ,['CRV','curve-dao-token']
-    ,['SBTC','sbtc']
-    ,['KROM','kromatika']
-    ,['DF','dforce-token']
-    ,['STG','stargate-finance']
-    ,['AELIN','aelin']
-    ,['RAI','rai']
-    ,['RETH','rocket-pool-eth']
-]
+# #defillama api feedback - only token symbols come through, makes it hard to map w/o doing it manually
+# coingecko_token_map = [
+#     ['LINK','chainlink']
+#     ,['USDT','tether']
+#     ,['USDC','usd-coin']
+#     ,['WETH','weth']
+#     ,['SUSD','nusd']
+#     ,['DAI','dai']
+#     ,['AAVE','aave']
+#     ,['WBTC','wrapped-bitcoin']
+#     ,['SNX','havven']
+#     ,['OP','optimism']
+#     ,['SETH','seth']
+#     ,['FXS','frax-share']
+#     ,['THALES','thales']
+#     ,['FRAX','frax']
+#     ,['ALUSD','alchemix-usd']
+#     ,['PERP','perpetual-protocol']
+#     ,['LUSD','liquity-usd']
+#     ,['LYRA','lyra-finance']
+#     ,['HND','hundred-finance']
+#     ,['BITANT','bitant']
+#     ,['UNI','uniswap']
+#     ,['SLINK','slink']
+#     ,['VELO','velodrome-finance']
+#     ,['DOLA','dola-usd']
+#     ,['CRV','curve-dao-token']
+#     ,['SBTC','sbtc']
+#     ,['KROM','kromatika']
+#     ,['DF','dforce-token']
+#     ,['STG','stargate-finance']
+#     ,['AELIN','aelin']
+#     ,['RAI','rai']
+#     ,['RETH','rocket-pool-eth']
+# ]
 
 
-# In[67]:
+# In[97]:
 
 
-cg_token_list = [i[0] for i in coingecko_token_map]
+# cg_token_list = [i[0] for i in coingecko_token_map]
+# # print(cg_token_list)
 
 
-# In[68]:
+# In[98]:
 
 
-# DISTINCT TOKENS
+# # DISTINCT TOKENS
 
-token_list = df_df[['token']].drop_duplicates()
-missing_token_list = token_list[~token_list['token'].isin(cg_token_list)]
-missing_token_list
-
-
-# In[69]:
+# token_list = df_df[['token']].drop_duplicates()
+# missing_token_list = token_list[~token_list['token'].isin(cg_token_list)]
+# missing_token_list
 
 
-
-cg_pds = []
-for t in coingecko_token_map:
-    cg_api = 'https://api.coingecko.com/api/v3/coins/'            + t[1] + '/market_chart?vs_currency=usd&days=max&interval=daily'
-    pr = pd.DataFrame( r.get(cg_api).json()['prices'] )
-    pr['token'] = t[0]
-    pr['cg_slug'] = t[1]
-    pr = pr.rename(columns={0:'date',1:'price_usd'})
-    pr['date'] = pd.to_datetime(pr['date'], unit ='ms') #convert to days
-    cg_pds.append(pr)
-
-cg_df = pd.concat(cg_pds)
-# cg_df
+# In[99]:
 
 
-# In[70]:
+
+# cg_pds = []
+# for t in coingecko_token_map:
+#     time.sleep(0.1)
+#     cg_api = 'https://api.coingecko.com/api/v3/coins/'\
+#             + t[1] + '/market_chart?vs_currency=usd&days=max&interval=daily'
+#     print(cg_api)
+#     pr = pd.DataFrame( r.get(cg_api).json()['prices'] )
+#     pr['token'] = t[0]
+#     pr['cg_slug'] = t[1]
+#     pr = pr.rename(columns={0:'date',1:'price_usd'})
+#     pr['date'] = pd.to_datetime(pr['date'], unit ='ms') #convert to days
+#     cg_pds.append(pr)
+
+# cg_df = pd.concat(cg_pds)
+# # cg_df
 
 
-data_df = df_df.merge(cg_df, on=['date','token'],how='inner')
+# In[107]:
+
+
+data_df = df_df.copy()#merge(cg_df, on=['date','token'],how='inner')
 data_df.sort_values(by='date',inplace=True)
-data_df['net_token_flow'] = data_df.groupby(['token','protocol','start_date'])['value'].apply(lambda x: x- x.shift(1))
+data_df['token_value'] = data_df['token_value'].replace(0, np.nan)
+data_df['price_usd'] = data_df['usd_value']/data_df['token_value']
+
+data_df.sort_values(by='date',inplace=True)
+
+data_df['last_token_value'] = data_df.groupby(['token','protocol'])['token_value'].shift(1)
+data_df['last_token_value'] = data_df['last_token_value'].fillna(0)
+
+data_df['net_token_flow'] = data_df['token_value'] - data_df['last_token_value']
+
 data_df['net_dollar_flow'] = data_df['net_token_flow'] * data_df['price_usd']
 
-data_df
+
+# display(data_df)
 
 
-# In[71]:
+# In[108]:
 
 
 data_df[data_df['protocol']=='perpetual-protocol'].sort_values(by='date')
 
 
-# In[72]:
+# In[109]:
 
 
 netdf_df = data_df[data_df['date']>= data_df['start_date']][['date','protocol','net_dollar_flow']]
@@ -176,7 +199,7 @@ netdf_df.reset_index(inplace=True)
 netdf_df
 
 
-# In[73]:
+# In[112]:
 
 
 fig = px.line(netdf_df, x="date", y="net_dollar_flow", color="protocol",              title="Daily Net Dollar Flow since Program Announcement",            labels={
@@ -216,11 +239,11 @@ cumul_fig.update_layout(
     legend_title="App Name",
 #     color_discrete_map=px.colors.qualitative.G10
 )
-cumul_fig.write_image(prepend + "img_outputs/svg/cumul_ndf.svg")
-cumul_fig.write_image(prepend + "img_outputs/png/cumul_ndf.png")
+cumul_fig.write_image(prepend + "img_outputs/svg/cumul_ndf.svg") #prepend + 
+cumul_fig.write_image(prepend + "img_outputs/png/cumul_ndf.png") #prepend + 
 
 
-# In[74]:
+# In[113]:
 
 
 # fig.show()
@@ -228,7 +251,7 @@ cumul_fig.write_image(prepend + "img_outputs/png/cumul_ndf.png")
 print("yay")
 
 
-# In[75]:
+# In[116]:
 
 
 get_ipython().system(' jupyter nbconvert --to python optimism_app_net_flows.ipynb')
