8323ea4bd3c1af143bde6381a401bf2d16dbf8d8
Michael Silberling <4006780+MSilb7@users.noreply.github.com>
2023-01-31 20:40:55 -0500

    wee

--- L2 TVL/optimism_pool_tvls.py
+++ L2 TVL/optimism_pool_tvls.py
@@ -195,10 +195,89 @@ def get_hop_pool_tvl(pid, min_ts = 0, max_ts = 99999999999999):
 # In[ ]:
 
 
+# Note, this is not in TVL tracking format - maybe we split this to a new file ~eventually
+def get_messari_sg_pool_snapshots(slug, chains = ['optimism'], min_ts = 0, max_ts = 99999999999999):
+        msr_dfs = []
+        print(slug)
+        for c in chains:
+                print(c)
+                try:
+                        # Set Chain
+                        curve = create_sg('https://api.thegraph.com/subgraphs/name/messari/' + slug + '-' + c)
+                        # Get Query
+                        q1 = curve.Query.liquidityPoolDailySnapshots(
+                        orderDirection='desc',
+                        first=1000,
+                                where=[
+                                curve.Query.liquidityPoolDailySnapshot.timestamp > min_ts,
+                                curve.Query.liquidityPoolDailySnapshot.timestamp <= max_ts,
+                                ]
+                        )
+                        # Pull Fields
+                        msr_list = sg.query_df([
+                                q1.id,
+                                q1.timestamp,
+                                q1.totalValueLockedUSD,
+                                q1.dailyVolumeUSD,
+                                q1.rewardTokenEmissionsUSD,
+                                #protocol
+                                q1.protocol.id,
+                                q1.protocol.name,
+                                q1.protocol.slug,
+                                q1.protocol.network,
+                                #pool
+                                q1.pool.id,
+                                q1.pool.name,
+                                q1.pool.symbol,
+                                q1.pool.inputTokens.id
+                        ]
+                        , pagination_strategy=ShallowStrategy)
+                        msr_df = pd.concat(msr_list)
+                except:
+                        msr_df = pd.DataFrame()
+                        continue
+                msr_dfs.append(msr_df)
+        
+        #combine all chains
+        msr_daily = pd.concat(msr_dfs)
+
+        #fix up column names
+
+        msr_daily.columns = msr_daily.columns.str.replace('liquidityPoolDailySnapshots_', '')
+
+        col_list = msr_daily.columns.to_list()
+        col_list.remove('pool_inputTokens_id') # we want to group by everything else 
+
+        print(col_list)
+        
+        msr_daily = msr_daily.fillna(0)
+
+        msr_daily = msr_daily.groupby(col_list).agg({'pool_inputTokens_id':lambda x: list(x)})
+        msr_daily.reset_index(inplace=True)
+
+        msr_daily['timestamp'] = pd.to_datetime(msr_daily['timestamp'],unit='s')
+        msr_daily['date'] = msr_daily['timestamp'].dt.floor('d')
+        msr_daily['id_rank'] = msr_daily.groupby(['id']).cumcount()+1
+        
+        return pd.DataFrame(msr_daily)
+
+
+# In[ ]:
+
+
 # pdf = get_curve_pool_tvl('0x061b87122ed14b9526a813209c8a59a633257bab')
 # vdf = get_velodrome_pool_tvl('0xfc77e39de40e54f820e313039207dc850e4c9e60')
 # get_hop_pool_tvl('SNX')
 # display(vdf)
+
+
+# In[ ]:
+
+
+# pd.
+
+# msr = get_messari_sg_pool_snapshots('curve-finance')
+# display(msr)
 
 
 # In[ ]:
